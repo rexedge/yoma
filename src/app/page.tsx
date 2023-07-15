89619eeaa4d85c289b4a10bbf3b237ad674839e8
LeoDivine <divineonyi2004@gmail.com>
2023-07-15 13:50:57 -0700

    Divine's Work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,53 @@
+import CardComponent from "./components/cardComponent";
+
 export default function Home() {
-	return (
-		<main className='flex h-screen w-screen items-center justify-center'>
-			Yoma Care
-		</main>
-	);
+  return (
+    <main className="flex-row w-[100%] items-center text-center mt-[40px] pb-[50px] justify-center">
+      <h6 className="font-bold text-[36px]">Top Carers</h6>
+      <div className="flex flex-wrap  justify-around w-[90%] m-auto">
+        <CardComponent
+          image="/imgSrc/first-person.jpg"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+        <CardComponent
+          image="/imgSrc/second-person.jpg"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+        <CardComponent
+          image="/imgSrc/third-person.jpg"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+        <CardComponent
+          image="/imgSrc/fourth-person.jpg"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+        <CardComponent
+          image="/imgSrc/fifth-person.webp"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+        <CardComponent
+          image="/imgSrc/sixth-person.webp"
+          name="Jane Doe"
+          careType="Dementia Care"
+          location="Lagos State, Nigeria"
+          recNo="39 Recommendations"
+        />
+      </div>
+    </main>
+  );
 }
