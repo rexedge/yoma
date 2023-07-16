6fc3a47ae8127ea016470dbb6f41e345ffe1e3f3
Ignatius Emeka <emekaignatius5@gmail.com>
2023-07-16 15:00:57 +0100

    first-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<main className='flex h-screen w-screen items-center justify-center'>
-			Yoma Care
-		</main>
-	);
+  return (
+    <main className='flex h-screen w-screen items-center justify-center'>
+      Yoma Care
+    </main>
+  );
 }
