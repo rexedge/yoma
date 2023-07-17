c3b067da636075c17cf7108e8d8da5cc27e897c2
LeoDivine <divineonyi2004@gmail.com>
2023-07-16 19:15:52 -0700

    New Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="flex-row w-[100%] items-center text-center mt-[40px] pb-[50px] justify-center">
       <h6 className="font-bold text-[36px]">Top Carers</h6>
-      <div className="flex flex-wrap  justify-around w-[90%] m-auto">
+      <div
+        className="flex-col w-[90%] m-auto lg:flex-row
+      md:w-[69%] lg:w-[90%] lg:flex lg:flex-wrap lg:justify-around xl:flex-row"
+      >
         <CardComponent
           image="/imgSrc/first-person.jpg"
           name="Jane Doe"
--- src/components/cardComponent.js
+++ src/components/cardComponent.js
@@ -6,12 +6,15 @@ export default function CardComponent({
   recNo,
 }) {
   return (
-    <div className="w-[30%] h-[450px] mt-[30px]">
+    <div className="w-[100%] h-[450px] mt-[30px] lg:w-[45%] xl:w-[30%]">
       <img
         className="rounded-t-[12px]  w-[100%] h-[300px] object-cover"
         src={image}
       />
-      <div className="w-[22%] h-[170px] mt-[-50px] text-black bg-white absolute overflow-hidden transition-all ease-in-out duration-[0.5s] hover:h-[220px]">
+      <div
+        className="w-[70%] h-[170px] mt-[-50px] text-black bg-white absolute overflow-hidden 
+      transition-all ease-in-out duration-[0.5s] hover:h-[220px] md:w-[60%] lg:w-[30%]  xl:w-[20%]"
+      >
         <h4 className="text-left pt-[16px] pl-[25px] text-[18px] font-bold">
           {name}
         </h4>
@@ -40,7 +43,7 @@ export default function CardComponent({
             />
             <p className="text-[16px] pl-[14px] mt-[2px]">{recNo}</p>
           </div>
-          <button className="w-[170px] h-[36px] bg-[#45214a] text-white text-[14px] rounded-[16px] mt-5 ml-[-89px]">
+          <button className="w-[170px] h-[36px] bg-[#45214a] text-white text-[14px] rounded-[16px] mt-5 ml-[-69px] md:ml-[-130px] lg:ml-[-77px]">
             Contact {name}
           </button>
         </div>
