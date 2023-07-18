b6fb67b04afa067249e1d3570862767116ec5cb2
Rexedge <ap.oyeniran@gmail.com>
2023-07-18 16:38:52 +0100

    Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,13 +62,13 @@ export default function Home() {
 				</div>
 			</Section>
 			<Section>
-				<div className='mx-auto justify-center text-center max-w-7xl w-screen bg-[#fefefe] px-0 py-16 w-[1728px] h-[676px] '>
+				<div className='mx-auto justify-center text-center max-w-7xl w-screen bg-[#fefefe] px-0 py-16 h-[676px] '>
 					<h3>How It Works</h3>
 				</div>
 				<HowItWorks />
 			</Section>
 			<Section>
-				<div className='mx-auto justify-center w-screen px-0 py-12 w-96 h-full'>
+				<div className='mx-auto justify-center w-screen px-0 py-12 h-full'>
 					<h1 className='text-2xl text-center text-[#020202] font-bold  sm:leading-tight sm:tracking-tight'>
 						Lets Care For You, Subscribe To A Care Plan!
 					</h1>
